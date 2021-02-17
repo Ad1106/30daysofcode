cb85a7297e5aedfb32eec4da64ac8407185edb63
Adarsh Shukla <shuklaadarsh1106@gmail.com>
2021-02-17 13:07:52 +0530

    Create Day 1: Data Types.py

---
+++ Day 1: Data Types.py
@@ -0,0 +1,18 @@
+
+
+        /* Declare second integer, double, and String variables. */
+        int i1=scan.nextInt();
+        double d1=scan.nextDouble();
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        scan.nextLine();
+        String str=scan.nextLine();
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+i1);
+        /* Print the sum of the double variables on a new line. */
+		System.out.println(d+d1);
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
+        System.out.println((s+str));
